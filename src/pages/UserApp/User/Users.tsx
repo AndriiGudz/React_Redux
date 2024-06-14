d24a9527f0c941de3d41f17fa983a8a2a4c990d0
Andrii Gudz <andrey.gudz@gmail.com>
2024-06-14 14:23:00 +0200

    Homeworks 16

--- src/pages/UserApp/User/Users.tsx
+++ src/pages/UserApp/User/Users.tsx
@@ -1,25 +1,42 @@
+import Button from "components/Button/Button"
 import { UsersPageWrapper, UserCard, Paragraph } from "./styles"
 
-import { useAppSelector } from "store/hooks"
-import { userSliceSelector } from "store/redux/userSlice/userSlice"
+import { useAppDispatch, useAppSelector } from "store/hooks"
+import {
+  userSliceSelector,
+  userSliceActions,
+} from "store/redux/userSlice/userSlice"
 import { v4 } from "uuid"
 
 function Users() {
-    const users = useAppSelector(userSliceSelector.users)
-    
-    const userCards = users.map((user) => {
-        return (
-            <UserCard key={user.id}>
-                <Paragraph>{user.fullName}</Paragraph>
-                <Paragraph>{user.age}</Paragraph>
-                <Paragraph>{user.jobTitle}</Paragraph>
-            </UserCard>
-        )
-    })
+  const users = useAppSelector(userSliceSelector.users)
+  const dispatch = useAppDispatch()
+
+  const handleDelete = (id: string) => {
+    dispatch(userSliceActions.deleteUser(id))
+  }
+
+  const handleDeleteAll = () => {
+    dispatch(userSliceActions.deleteAllUsers())
+  }
+
+  const userCards = users.map(user => {
+    return (
+      <UserCard key={user.id}>
+        <Paragraph>{user.fullName}</Paragraph>
+        <Paragraph>{user.age}</Paragraph>
+        <Paragraph>{user.jobTitle}</Paragraph>
+        <Button name="Удалить" onClick={() => handleDelete(user.id)} />
+      </UserCard>
+    )
+  })
 
   return (
     <UsersPageWrapper>
-   {userCards}
+      {userCards}
+      {users.length > 0 && (
+         <Button name="Удалить всех пользователей" onClick={handleDeleteAll} />
+      )}
     </UsersPageWrapper>
   )
 }
--- src/store/redux/userSlice/userSlice.ts
+++ src/store/redux/userSlice/userSlice.ts
@@ -1,21 +1,33 @@
-import { createAppSlice } from "store/createAppSlice";
-import { UsersSliceState, UserData } from "./types";
-import { create } from "domain";
-import { PayloadAction } from "@reduxjs/toolkit";
+import { createAppSlice } from "store/createAppSlice"
+import { UsersSliceState, UserData } from "./types"
+import { create } from "domain"
+import { PayloadAction } from "@reduxjs/toolkit"
 
 const usersInitialState: UsersSliceState = {
-    users: []
+  users: [],
 }
 
 export const userSlice = createAppSlice({
-    name: 'USERS',
-    initialState: usersInitialState,
-    reducers: create => ({
-      addUser: create.reducer((state: UsersSliceState, action: PayloadAction<UserData>) => {state.users = [...state.users, action.payload]})  
+  name: "USERS",
+  initialState: usersInitialState,
+  reducers: create => ({
+    addUser: create.reducer(
+      (state: UsersSliceState, action: PayloadAction<UserData>) => {
+        state.users = [...state.users, action.payload]
+      },
+    ),
+    deleteUser: create.reducer(
+      (state: UsersSliceState, action: PayloadAction<string>) => {
+        state.users = state.users.filter(user => user.id !== action.payload)
+      },
+    ),
+    deleteAllUsers: create.reducer((state: UsersSliceState) => {
+      state.users = []
     }),
-    selectors: {
-        users: (state: UsersSliceState) => state.users
-    }
+  }),
+  selectors: {
+    users: (state: UsersSliceState) => state.users,
+  },
 })
 
 export const userSliceActions = userSlice.actions
